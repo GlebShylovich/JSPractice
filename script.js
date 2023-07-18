7686778ebbd0d21392cc8ddde28f0c526824c8ad
Gleb Shylovich <97163965+GlebShylovich@users.noreply.github.com>
2023-07-18 10:43:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const container = document.querySelector(".container");
+
+function getData() {
+  fetch("https://restcountries.com/v3.1/all")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      for (var i = 0; i < data.length; i += 5) {
+        const flag = data[i].flags.png;
+        const country = data[i].name.official;
+        const region = data[i].region;
+        const population = Math.round(data[i].population / 1e4);
+        const language = Object.values(data[i].languages);
+        const currency = Object.values(Object.values(data[i].currencies)[0]);
+        console.log(currency);
+        createCard(data, flag, country, region, population, language, currency);
+      }
+    });
+}
+
+getData();
+
+function createCard(data, flag, country, region, population, language, currency) {
+  const card = document.createElement("div");
+  const template = `
+    <div class="col">
+        <div class="card h-100">
+            <img src="${flag}" class="card-img-top" alt="flag">
+            <div class="card-body">
+                <h1>${country}</h1>
+                <p>${region}</p>
+                <p>&#128106: ${population}K</p>
+                <p>&#128483: ${language}</p>
+                <p>&#128176: ${currency}</p>
+            </div>
+        </div>
+    </div>
+    `;
+    for (var i = 0; i < data.length; i++) {
+      card.innerHTML = template;
+      container.append(card);
+    }
+}
